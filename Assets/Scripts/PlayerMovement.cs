1e2e882bc2a538370c8101e912cdaea7865da56a
cm600286 <91765679+cm600286@users.noreply.github.com>
2022-08-06 11:25:37 -0700

    Added Spaceship-like movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float accel = 100;
+    [SerializeField] float rotateAccel = 0;
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
@@ -16,12 +17,12 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.UpArrow))
-        {
-            rb.AddForce(Vector2.up * (accel));
-            Debug.Log("Pressed");
-        }
-        int dir = Input.GetKey(KeyCode.LeftArrow) ? -1 : 0;
-        dir += Input.GetKey(KeyCode.RightArrow) ? 1 : 0;
-        rb.AddForce(Vector2.right * dir * accel / 5);
+            rb.AddForce(Quaternion.Euler(0, 0, rb.rotation) * Vector3.up * accel);
+        int dir = Input.GetKey(KeyCode.LeftArrow) ? 1 : 0;
+        dir += Input.GetKey(KeyCode.RightArrow) ? -1 : 0;
+        if (rotateAccel == 0)
+            rb.AddForce(Vector3.left * dir * accel / 5);
+        else
+            rb.rotation += dir * rotateAccel;
     }
 }
