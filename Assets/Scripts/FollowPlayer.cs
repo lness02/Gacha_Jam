decf581bdc8b1c7bdcb5c41cfa7c1156a74b53b9
cm600286 <91765679+cm600286@users.noreply.github.com>
2022-08-07 09:30:59 -0700

    Fixed jump height

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -7,6 +7,8 @@ public class FollowPlayer : MonoBehaviour
     private bool connected = true;
     private Vector3 difference;
     [SerializeField] GameObject player;
+    // speed is between 0 (non-moving) and 1 (fast camera)
+    [SerializeField] float speed = 0.05f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +21,7 @@ public class FollowPlayer : MonoBehaviour
     {
         if (connected)
         {
-            this.transform.position += (player.transform.position - this.transform.position - difference)/50;
+            this.transform.position += (player.transform.position - this.transform.position - difference) * speed;
         }
         if (Input.GetKeyDown(KeyCode.Space)) disconnect();
         else if (Input.GetKeyUp(KeyCode.Space)) reconnect();
